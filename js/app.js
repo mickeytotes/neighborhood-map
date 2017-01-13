538e203e4ee0301618d6009907a14203d0996f1b
Michael Satow <satow.m@gmail.com>
2017-01-13 16:20:56 -0500

    init map

--- js/app.js
+++ js/app.js
@@ -1,1 +1,10 @@
 // Make dat map doe!
+
+var map;
+
+var initMap = function() {
+	map = new google.maps.Map(document.getElementById('map'), {
+		center: {lat: 40.843436, lng: -73.934794},
+		zoom: 14
+	});
+};
