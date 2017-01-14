7efa54ae831922c6305c9dcb8e16fe0eea954d2c
Michael Satow <satow.m@gmail.com>
2017-01-14 12:12:40 -0500

    change http to https

--- js/app.js
+++ js/app.js
@@ -146,8 +146,8 @@ function fillInfoWindow(marker, infowindow) {
       // origin of 0, 0 and be anchored at 10, 34. Again, thank you Google API for the help.
       function makeMarkerIcon(markerColor) {
         var markerImage = new google.maps.MarkerImage(
-          'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|' + markerColor +
-          '|40|_|%E2%80%A2',
+          "http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|" + markerColor +
+          "|40|_|%E2%80%A2",
           new google.maps.Size(21, 34),
           new google.maps.Point(0, 0),
           new google.maps.Point(10, 34),
