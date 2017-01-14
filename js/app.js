14b020f5cc0829b3cb6d80e89657f6744e6b07b8
Michael Satow <satow.m@gmail.com>
2017-01-14 08:23:35 -0500

    add model

--- js/app.js
+++ js/app.js
@@ -1,5 +1,40 @@
 // Make dat map doe!
 
+
+var model = [
+	{
+		title: "Saggio",
+		location: {lat: 40.851473, lng: -73.939588},
+		type: "Restaurant"
+	},
+    {
+    	title: "Mambi Steakhouse",
+    	location: {lat: 40.84772, lng: -73.938251},
+    	type: "Restaurant"
+    },
+    {
+    	title: "Planet Fitness",
+    	location: {lat: 40.847326, lng: -73.937638},
+    	type: "Gym"
+    },
+    {
+    	title: "Le Cheile",
+    	location: {lat: 40.851495, lng: -73.939988},
+    	type: "Restaurant"
+    },
+    {
+    	title: "Elite Cleaners",
+    	location: {lat: 40.843097, lng: -73.939438},
+    	type: "Dry Cleaners"
+    },
+    {
+    	title: "Columbia Wine Co",
+    	location: {lat: 40.842125, lng: -73.939272},
+    	type: "Liquor Store"
+    }
+]
+
+
 var map;
 
 var initMap = function() {
