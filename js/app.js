27899019827008864c5a2b0d66b8d45f040f273a
Michael Satow <satow.m@gmail.com>
2017-01-17 12:38:30 -0500

    add if statement for undefined

--- js/app.js
+++ js/app.js
@@ -85,7 +85,10 @@ var ViewModel = function() {
 	this.filterList = ko.computed(function() {
 		var listPlaces = self.places();
 		for (var i = 0; i < listPlaces.length; i++) {
-			if (self.selectedType() !== listPlaces[i].type()) {
+			if (self.selectedType() == undefined) {
+				listPlaces[i].isVisible(true);
+			}
+			else if (self.selectedType() !== listPlaces[i].type()) {
 				listPlaces[i].isVisible(false);
 			} else {
 				listPlaces[i].isVisible(true);
