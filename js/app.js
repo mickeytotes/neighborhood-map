b5c9214c12e0df46c6a084cb51a23b076f0ebdec
Michael Satow <satow.m@gmail.com>
2017-01-17 08:44:01 -0500

    change self to this

--- js/app.js
+++ js/app.js
@@ -75,7 +75,7 @@ var ViewModel = function() {
 	this.options = ["Liquor Store", "Restaurant", "Dry Cleaners", "Gym"];
 
 	//this is where I left off
-	self.filterList = ko.computed(function() {
+	this.filterList = ko.computed(function() {
 
 		//for (var i = 0; i < places[i]) {
 		//	if ()
