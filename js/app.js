4ab669066e1a6438aebc1b7dd1680171b392469b
Michael Satow <satow.m@gmail.com>
2017-01-17 13:55:43 -0500

    make marker property of spots

--- js/app.js
+++ js/app.js
@@ -48,6 +48,139 @@ var neighborhoodSpots = {
     	]
 	}
 
+// ******** MAP ******** //
+
+
+var map;
+
+// Create empty arry to push markers into.
+var markers = [];
+
+// Get that map going in the map div!
+// Thanks to Google Maps API for the super helpful documentation, course, and instructions!
+var initMap = function() {
+	map = new google.maps.Map(document.getElementById('map'), {
+		center: {lat: 40.843436, lng: -73.934794},
+		zoom: 14
+	});
+
+	// Close infowindow when any other portion of the map is clicked.
+	google.maps.event.addListener(map, "click", function(event) {
+		mapInfoWindow.close();
+		marker.setIcon(defaultIcon);
+	});
+
+	var mapInfoWindow = new google.maps.InfoWindow();
+
+	var defaultIcon = makeMarkerIcon('ff2d00');
+
+	var highlightedIcon = makeMarkerIcon('631504');
+
+	var clickedIcon = makeMarkerIcon('46e91f');
+
+	// loop through neighborhoodSpots array to make markers.
+	for (var i = 0; i < neighborhoodSpots.spots.length; i++) {
+		var title = neighborhoodSpots.spots[i].title;
+		var phone = neighborhoodSpots.spots[i].phone;
+		var position = neighborhoodSpots.spots[i].location;
+		var id = neighborhoodSpots.spots[i].foursquareID;
+
+		var marker = new google.maps.Marker({
+			map: map,
+			position: position,
+			title: title,
+			phone: phone,
+			// markers drop in when page loads.
+			animation: google.maps.Animation.DROP,
+			icon: defaultIcon,
+			id: id
+		});
+
+
+/*	// Make marker bounce on click.
+	marker.addListener("click", makeBounce); */
+
+	neighborhoodSpots.spots[i].marker = marker;
+
+	// Push markers to markers[] array.
+	markers.push(marker);
+
+	//Open info window on click.
+	marker.addListener("click", function() {
+		fillInfoWindow(this, mapInfoWindow);
+		// change clicked icon's color.
+		this.setIcon(clickedIcon);
+	});
+
+	// Change marker color when mouse hovers over marker.
+	marker.addListener("mouseover", function() {
+		this.setIcon(highlightedIcon);
+	});
+
+	// Use default color when not hovering over marker.
+	marker.addListener("mouseout", function() {
+		this.setIcon(defaultIcon);
+
+		// Make sure icon hasn't been clicked.
+		//if (this.setIcon(clickedIcon) = true) {
+		//this.setIcon(clickedIcon);
+		//} else {
+		//	this.setIcon(defaultIcon);
+		//}
+	});
+}
+
+/*	function makeBounce() {
+		var bounce = setInterval(function() {
+			marker.setAnimation(google.maps.Animation.BOUNCE);
+		}, 100);
+
+		setTimeout(function() {
+			clearInterval(bounce);
+		}, 1000);
+	} */
+
+	function fillInfoWindow(marker, infowindow) {
+		// Check if marker's window is already open.
+		if (infowindow.marker != marker) {
+			// Clear infowindow
+			infowindow.setContent('');
+			infowindow.marker = marker;
+			// Open  infowinow
+			infowindow.open(map, marker);
+
+			// Close infowindow on click.
+			infowindow.addListener('closeclick', function() {
+				infowindow.marker(null);
+				// Change icon color back to default.
+				marker.setIcon(defaultIcon);
+			});
+
+			// Uncomment this once ajax request is built to get venue info for the infowindow.
+			//getFSInfo();
+
+			infowindow.setContent("<div>" + marker.title + "</div><div>" + marker.phone + "</div>");
+
+			}
+		}
+
+		// This function takes in a color, and then creates a new marker
+        // icon of that color. The icon will be 21px wide by 34px high, have an
+        // origin of 0, 0 and be anchored at 10, 34. Again, thank you Google API for the help.
+      function makeMarkerIcon(markerColor) {
+        var markerImage = new google.maps.MarkerImage(
+          "https://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|" + markerColor +
+          "|40|_|%E2%80%A2",
+          new google.maps.Size(21, 34),
+          new google.maps.Point(0, 0),
+          new google.maps.Point(10, 34),
+          new google.maps.Size(21,34));
+        return markerImage;
+      }
+};
+
+
+
 	//****** VIEW ******** //
 
 var listView = function(data) {
@@ -55,6 +188,7 @@ var listView = function(data) {
 	// Make location an observable in case this needs to be changed
 	this.location = ko.observable(data.location);
 	this.type = ko.observable(data.type);
+	this.marker = ko.observable(data.marker);
 	// Create property for visible binding.
 	this.isVisible = ko.observable(true);
 };
@@ -134,131 +268,3 @@ $.ajax({
 });
 
 }*/
-// ******** MAP ******** //
-
-
-var map;
-
-// Create empty arry to push markers into.
-var markers = [];
-
-// Get that map going in the map div!
-// Thanks to Google Maps API for the super helpful documentation, course, and instructions!
-var initMap = function() {
-	map = new google.maps.Map(document.getElementById('map'), {
-		center: {lat: 40.843436, lng: -73.934794},
-		zoom: 14
-	});
-
-	// Close infowindow when any other portion of the map is clicked.
-	google.maps.event.addListener(map, "click", function(event) {
-		mapInfoWindow.close();
-		marker.setIcon(defaultIcon);
-	});
-
-	var mapInfoWindow = new google.maps.InfoWindow();
-
-	var defaultIcon = makeMarkerIcon('ff2d00');
-
-	var highlightedIcon = makeMarkerIcon('631504');
-
-	var clickedIcon = makeMarkerIcon('46e91f');
-
-	// loop through neighborhoodSpots array to make markers.
-	for (var i = 0; i < neighborhoodSpots.spots.length; i++) {
-		var title = neighborhoodSpots.spots[i].title;
-		var phone = neighborhoodSpots.spots[i].phone;
-		var position = neighborhoodSpots.spots[i].location;
-		var id = neighborhoodSpots.spots[i].foursquareID;
-
-		var marker = new google.maps.Marker({
-			map: map,
-			position: position,
-			title: title,
-			phone: phone,
-			// markers drop in when page loads.
-			animation: google.maps.Animation.DROP,
-			icon: defaultIcon,
-			id: id
-		});
-
-
-/*	// Make marker bounce on click.
-	marker.addListener("click", makeBounce); */
-
-	// Push markers to markers[] array.
-	markers.push(marker);
-
-	//Open info window on click.
-	marker.addListener("click", function() {
-		fillInfoWindow(this, mapInfoWindow);
-		// change clicked icon's color.
-		this.setIcon(clickedIcon);
-	});
-
-	// Change marker color when mouse hovers over marker.
-	marker.addListener("mouseover", function() {
-		this.setIcon(highlightedIcon);
-	});
-
-	// Use default color when not hovering over marker.
-	marker.addListener("mouseout", function() {
-		this.setIcon(defaultIcon);
-
-		// Make sure icon hasn't been clicked.
-		//if (this.setIcon(clickedIcon) = true) {
-		//this.setIcon(clickedIcon);
-		//} else {
-		//	this.setIcon(defaultIcon);
-		//}
-	});
-}
-
-/*	function makeBounce() {
-		var bounce = setInterval(function() {
-			marker.setAnimation(google.maps.Animation.BOUNCE);
-		}, 100);
-
-		setTimeout(function() {
-			clearInterval(bounce);
-		}, 1000);
-	} */
-
-	function fillInfoWindow(marker, infowindow) {
-		// Check if marker's window is already open.
-		if (infowindow.marker != marker) {
-			// Clear infowindow
-			infowindow.setContent('');
-			infowindow.marker = marker;
-			// Open  infowinow
-			infowindow.open(map, marker);
-
-			// Close infowindow on click.
-			infowindow.addListener('closeclick', function() {
-				infowindow.marker(null);
-				// Change icon color back to default.
-				marker.setIcon(defaultIcon);
-			});
-
-			// Uncomment this once ajax request is built to get venue info for the infowindow.
-			//getFSInfo();
-
-			infowindow.setContent("<div>" + marker.title + "</div><div>" + marker.phone + "</div>");
-
-			}
-		}
-
-		// This function takes in a color, and then creates a new marker
-        // icon of that color. The icon will be 21px wide by 34px high, have an
-        // origin of 0, 0 and be anchored at 10, 34. Again, thank you Google API for the help.
-      function makeMarkerIcon(markerColor) {
-        var markerImage = new google.maps.MarkerImage(
-          "https://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|" + markerColor +
-          "|40|_|%E2%80%A2",
-          new google.maps.Size(21, 34),
-          new google.maps.Point(0, 0),
-          new google.maps.Point(10, 34),
-          new google.maps.Size(21,34));
-        return markerImage;
-      }
-};
